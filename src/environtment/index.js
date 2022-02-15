95651bb94f225bd172f980c46db4cc348ddbe6cf
fikar46 <zulfikar@doku.com>
2022-02-15 20:53:52 +0700

    add env

--- src/environtment/index.js
+++ src/environtment/index.js
@@ -1,8 +1,8 @@
 var api;
 export const storage = 'https://storage.siapptn.com';
 if(window.location.hostname == 'localhost'){
-     api = 'https://api.meylendra.com:2023';
+     api = 'https://zkeys.id:2023';
 }else{
-     api = 'https://api.meylendra.com:2023';
+     api = 'https://zkeys.id:2023';
 }
 export const koneksi = `${api}`
